6d061af2ff56bb2b8e4e628cfea03c463d838611
ndean <nazakoglu@gradcenter.cuny.edu>
2018-03-20 13:23:52 -0400

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,11 +1,10 @@
-class Solution(Object):
-  def twoSum(self, nums, target):
-  hash_map = {}
-  for idx, value in enumerate(nums):
-    hash_map[value]=idx
-  for idx1, value in enumerate(nums):
-    if target-value in hash_map:
-      idx2 = hash_map[target-value]
-      if idx1 != idx2:
-        return [idx1+1, idx2+1]
-    
+class Solution(object):
+    def twoSum(self, nums, target):
+        hash_map = {}
+        for idx, value in enumerate(nums):
+            hash_map[value] = idx
+        for idx1, value in enumerate(nums):
+            if target - value in hash_map:
+                idx2 = hash_map[target - value]
+                if idx1 != idx2:
+                    return [idx1 + 1, idx2 + 1]
